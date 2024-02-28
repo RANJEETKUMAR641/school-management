5a952efb6e0c0bce0abd5b4023f7dd0389ff5a49
RANJEETKUMAR641 <79505038+RANJEETKUMAR641@users.noreply.github.com>
2024-02-28 13:55:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./component/Login/Login";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Register from "./component/Register/Register";
+import Main from "./component/Main/Main";
+import Course from './component/ContentPages/Course'
+import Students from "./component/ContentPages/Students";
+import Attendance from "./component/ContentPages/Attendance";
+import Schedule from "./component/ContentPages/Schedule";
+import Fees from "./component/ContentPages/Fees";
+import Holidays from "./component/ContentPages/Holidays";
+import Transport from "./component/ContentPages/Transport";
+import Notify  from './component/ContentPages/Notify'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/main" element={<Main />} />
+          <Route path="/course" element={<Course/>}/>
+          <Route path="/students" element={<Students/>}/>
+          <Route path="/attendance" element={<Attendance/>}/>
+          <Route path="/schedule" element={<Schedule/>}/>
+          <Route path="/fees" element={<Fees/>}/>
+          <Route path="/holidays" element={<Holidays/>}/>
+          <Route path="/transport" element={<Transport/>}/>
+          <Route path="/notification" element={<Notify/>}/>
+          
+
+
+
+
+
+
+
+
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
